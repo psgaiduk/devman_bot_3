42480edbc0469b8ad6ece386e714f2247a68903c
pavel <psgaiduk@gmail.com>
2022-02-13 06:10:52 +0300

    add work from ConvesationHandler

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,15 +1,19 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram import ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, RegexHandler
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 import os
 from dotenv import load_dotenv
 from create_question_answer import get_questions_and_answers
 import redis
+
+QUIZ = range(1)
 
 
 def start(bot, update):
     custom_keyboard = [['New question', 'Surrender'], ['My Score']]
     reply_markup = ReplyKeyboardMarkup(custom_keyboard)
     bot.send_message(chat_id=update.message.chat.id, text="Custom Keyboard Test", reply_markup=reply_markup)
+
+    return QUIZ
 
 
 def help(bot, update):
@@ -35,12 +39,42 @@ def quiz(bot, update):
         update.message.reply_text(text)
 
 
+def handle_new_question_request(bot, update):
+    redis_password = os.environ['REDIS_PASSWORD']
+    r = redis.Redis(host='redis-19360.c240.us-east-1-3.ec2.cloud.redislabs.com', port=19360, db=0,
+                    password=redis_password, decode_responses=True)
+    question, answer = get_questions_and_answers()
+    r.set(update.message.chat.id, answer)
+    bot.send_message(chat_id=update.message.chat.id, text=question)
+
+
+def handle_solution_attempt(bot, update):
+    redis_password = os.environ['REDIS_PASSWORD']
+    r = redis.Redis(host='redis-19360.c240.us-east-1-3.ec2.cloud.redislabs.com', port=19360, db=0,
+                    password=redis_password, decode_responses=True)
+    text = 'Неправильно… Попробуешь ещё раз?'
+    answer = r.get(update.message.chat.id)
+    if update.message.text == answer:
+        text = 'Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»'
+
+    update.message.reply_text(text)
+
+
 # def error(bot, update, error):
 #     """Log Errors caused by Updates."""
 #     logger.warning('Update "%s" caused error "%s"', update, error)
 
 
+def cancel(bot, update):
+    user = update.message.from_user
+    update.message.reply_text('Bye! I hope we can talk again some day.',
+                              reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
+
+
 def main():
+
     dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
     if os.path.exists(dotenv_path):
         load_dotenv(dotenv_path)
@@ -50,22 +84,18 @@ def main():
     updater = Updater(token_telegram)
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            QUIZ: [
+                RegexHandler('^(New question)$', handle_new_question_request),
+                MessageHandler(Filters.text, handle_solution_attempt)
+            ],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)]
+    ))
 
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, quiz))
-
-    # log all errors
-    # dp.add_error_handler(error)
-
-    # Start the Bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
