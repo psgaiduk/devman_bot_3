479622823219a3ad22315abeffab5b2e95f28dbf
pavel <psgaiduk@gmail.com>
2022-02-20 11:20:09 +0300

    delete field last question id from db

--- redis_work.py
+++ redis_work.py
@@ -7,10 +7,11 @@ class RedisDB:
 
     def __init__(self, host, port, password):
         self.r = redis.Redis(host=host, port=port, db=0, password=password, decode_responses=True)
-        last_question_id = self.r.get('last_question_id')
+        last_question_id = len(self.r.keys(pattern='question_*'))
         if not last_question_id:
-            self.r.set('last_question_id', 0)
-            self.add_question('Как у тебя дела?\nОтвет: Всё хорошо', 'Всё хорошо')
+            self.add_question('Как у тебя дела?\n'
+                              'Сообщите администратору, что база вопросов пуста'
+                              '\nОтвет: Всё хорошо', 'Всё хорошо')
 
     def get_user(self, chat_id):
         user_data = self.r.get(chat_id)
@@ -31,10 +32,9 @@ class RedisDB:
         }))
 
     def add_question(self, question, answer):
-        last_question_id = int(self.r.get('last_question_id')) + 1
+        last_question_id = len(self.r.keys(pattern='question_*')) + 1
         question_answer = {'question': question, 'answer': answer}
         self.r.set(f'question_{last_question_id}', json.dumps(question_answer))
-        self.r.set('last_question_id', last_question_id)
 
     def get_question_and_answer(self, question_id):
         data = self.r.get(question_id)
@@ -44,6 +44,6 @@ class RedisDB:
         return question, answer
 
     def get_random_question(self):
-        last_question_id = int(self.r.get('last_question_id'))
+        last_question_id = len(self.r.keys(pattern='question_*'))
         question_id = random.randint(1, last_question_id + 1)
         return question_id, self.get_question_and_answer(f'question_{question_id}')[0]
