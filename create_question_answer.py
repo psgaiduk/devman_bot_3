272c1ba6eaafe9858d4886e89072a3ecb3731d3e
Pavel <psgaiduk@gmail.com>
2022-02-23 18:06:19 +0300

    added delete file if it was added to db

--- create_question_answer.py
+++ create_question_answer.py
@@ -1,10 +1,9 @@
 import os
-import random
 from redis_work import RedisDB
 from dotenv import load_dotenv
 
 
-def add_question_and_answer_to_db(file_name):
+def add_question_and_answer_to_db():
     dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
     if os.path.exists(dotenv_path):
         load_dotenv(dotenv_path)
@@ -15,22 +14,21 @@ def add_question_and_answer_to_db(file_name):
     r = RedisDB(host=redis_host, port=redis_port, password=redis_password)
 
     questions_dir = os.path.join(os.path.dirname(__file__), 'questions')
+    files = os.listdir(questions_dir)
+    for file_name in files:
+        file_path = os.path.join(questions_dir, file_name)
+        with open(file_path, encoding='KOI8-R') as file_with_questions:
+            questions = file_with_questions.read().split('Вопрос')[1:]
 
-    file_path = os.path.join(questions_dir, file_name)
-    with open(file_path, encoding='KOI8-R') as file_with_questions:
-        questions = file_with_questions.read()
+        for question_and_answer in questions:
+            question, answer = question_and_answer.split('Ответ:')
+            question = question.split(':')[1].strip()
+            answer = answer.split('\n\n')[0].strip()
 
-    question_and_answer = ''
-    while 'Ответ:' not in question_and_answer:
-        list_questions = questions.split('Вопрос')[1:]
-        question_and_answer = random.choice(list_questions)
+            r.add_question(question, answer)
 
-    question_and_answer = question_and_answer.split('Ответ:')
-    question = question_and_answer[0].split(':')[1].strip()
-    answer = question_and_answer[1].split('\n\n')[0].strip()
-
-    r.add_question(question, answer)
+        os.remove(file_path)
 
 
 if __name__ == '__main__':
-    add_question_and_answer_to_db(file_name=input('input file name (.txt)'))
+    add_question_and_answer_to_db()
